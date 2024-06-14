8d79965329751ac0b5f09a342440c835060c470c
Artem <artem15stepanenko@outlook.com>
2024-06-15 00:56:03 +0300

    introducedTilePallete

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 using Zenject;
 
@@ -9,6 +10,8 @@ public class PlayerInstaller : MonoInstaller
 
     [SerializeField] private GameObject _playerPrefab;
 
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+
     public override void InstallBindings()
     {
         var playerInstance = Container.InstantiatePrefab(_playerPrefab, _playerSpawnPoint, _playerRotation, null);
@@ -16,5 +19,8 @@ public class PlayerInstaller : MonoInstaller
         Container.BindInstance(_playerPrefab).NonLazy();
         Container.Bind<PlayerHealth>().FromInstance(playerInstance.GetComponent<PlayerHealth>());
         Container.Bind<PlayerPicker>().FromInstance(playerInstance.GetComponent<PlayerPicker>());
+
+        _virtualCamera.Follow = playerInstance.transform;
+        _virtualCamera.LookAt = playerInstance.transform;
     }
 }
